a31812afc454a469d3e3e20f4b3cbde9639cdd3e
Rígille Scherrer Borges Menezes <rigillesbmenezes@protonmail.com>
2020-12-20 16:46:48 -0300

    atacar gauss_trick
    ele resiste.

--- gauss_trick.v
+++ gauss_trick.v
@@ -25,7 +25,7 @@ Lemma list_sum_rev : forall l,
 Proof.
   induction l as [|h t IH].
   - reflexivity.
-  - simpl. rewrite list_sum_app. rewrite IH. Search (_ + _ = _ + _).
+  - simpl. rewrite list_sum_app. rewrite IH.
   rewrite Nat.add_comm. simpl. rewrite <- plus_n_O.
   reflexivity.
 Qed.
@@ -34,12 +34,44 @@ Example gauss_trick_ex :
   vec_sum (rev (seq 0 3)) (seq 0 3) = repeat (pred 3) 3.
 Proof. simpl. reflexivity. Qed.
 
-Lemma gauss_trick : forall n,
-  vec_sum (rev (seq 0 n)) (seq 0 n) = repeat (pred n) n.
-Admitted.
-
 Lemma repeat_sum : forall n m,
   list_sum (repeat n m) = m*n.
+Proof.
+  induction m as [|m' IHm'].
+  - reflexivity.
+  - simpl. rewrite IHm'. reflexivity.
+Qed.
+
+Fixpoint dec_seq n :=
+  match n with
+  | O => nil
+  | S n' => n' :: dec_seq n'
+  end.
+
+Lemma seq_first : forall n,
+  [0] ++ seq 1 n = seq 0 (S n).
+Proof. reflexivity. Qed.
+
+Lemma seq_last: forall n,
+  seq 0 n ++ [n] = seq 0 (S n).
+Proof.
+  induction n as [|n' IHn'].
+  - reflexivity.
+  - simpl.
+Admitted.
+Lemma rev_seq_dec_seq : forall n,
+  rev (seq 0 n) = dec_seq n.
+Proof.
+  induction n as [|n' IHn'].
+  - reflexivity.
+  - simpl.
+  rewrite <- IHn'. simpl. Search (rev (_ ++ _)).
+  replace ([0]) with (rev [0]) by reflexivity.
+  rewrite <- rev_app_distr. rewrite <- rev_unit.
+Admitted.
+
+Lemma gauss_trick : forall n,
+  vec_sum (rev (seq 0 n)) (seq 0 n) = repeat (pred n) n.
 Admitted.
 
 Theorem sum_consecutive : forall n,
